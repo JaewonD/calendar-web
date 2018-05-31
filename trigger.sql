5313d88e581780537e7807f8b20dd4ef5e64f588
Jaeyoon Kim <kimjy2630@naver.com>
2018-05-31 11:16:34 +0900

    merged uid-gid not null trigger and added tiem trigger

--- trigger.sql
+++ trigger.sql
@@ -7,7 +7,7 @@ DELIMITER $$
 CREATE TRIGGER Schedules_BI01_TG BEFORE INSERT ON Schedules
 FOR EACH ROW
 BEGIN
-    IF NEW.user_id IS NULL AND NEW.group_id IS NULL THEN
+    IF (NEW.user_id IS NULL AND NEW.group_id IS NULL) OR (NEW.user_id IS NOT NULL AND NEW.group_id IS NOT NULL) THEN
         SIGNAL SQLSTATE '12345'
             SET MESSAGE_TEXT = 'One and only one of Schedules.user_id and Schedules.group_id is NULL.';
     END IF;
@@ -16,9 +16,9 @@ END$$
 CREATE TRIGGER Schedules_BI02_TG BEFORE INSERT ON Schedules
 FOR EACH ROW
 BEGIN
-    IF NEW.user_id IS NOT NULL AND NEW.group_id IS NOT NULL THEN
+    IF NEW.start_time > NEW.end_time THEN
         SIGNAL SQLSTATE '12345'
-            SET MESSAGE_TEXT = 'One and only one of Schedules.user_id and Schedules.group_id is NULL.';
+            SET MESSAGE_TEXT = 'Start time should be earlier than end time.';
     END IF;
 END$$
 
