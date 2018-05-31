7edabacc79bfbc77bacaf68da143cc878c6a36d2
Jaewon Jung <wjdwodnjs147@kaist.ac.kr>
2018-05-31 23:06:39 +0900

    Implement login, logout, schedule get api

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
 
+var session = require('express-session');
+
+app.use(session({
+ secret: '@#@$MYSIGN#@$#$',
+ resave: false,
+ saveUninitialized: true
+}));
+
 app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
-app.use('/api', require('./api/user'))
+app.use('/api/user/', require('./api/user'))
+app.use('/api/schedule', require('./api/schedule'))
 
 app.engine('html', require('ejs').renderFile);
 
