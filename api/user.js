90abd67150985f5edc324e90b6ff2196639028b1
Jaewon Jung <wjdwodnjs147@kaist.ac.kr>
2018-06-04 16:22:20 +0900

    Implement signup

--- api/user.js
+++ api/user.js
@@ -58,6 +58,27 @@ router.get('/logout', (req, res) => {
     });
     res.json({"success":"true"});
     return;
-})
+});
+
+router.post('/signup/:username/:password', (req, res) => {
+    var username = req.params.username;
+    var password = req.params.password;
+    console.log("Signup request arrived");
+
+    var response = {};
+    pool.query(util.format('INSERT INTO Users(name, pwd) VALUES ("%s", "%s")', username, password), function(err, results, fields) {
+        var response = {};
+        if (!err) {
+            response["success"] = "true";
+            response["error"] = "";
+            res.json(response);
+        } else {
+            console.log(err)
+            response["success"] = "false";
+            response["error"] = "Internal signup server error!";
+            res.json(response);
+        }
+    });
+});
 
 module.exports = router;
---
+++ public/js/signup.js
@@ -0,0 +1,31 @@
+var domain = "http://localhost:3000"
+$('#signup-form').submit(function() {
+    var input_username = $('#usernameInput').val();
+    var input_password = $('#passwordInput').val();
+    var input_password_confirm = $('#passwordInputConfirm').val();
+
+    if (input_password != input_password_confirm) {
+        alert("Password and confirm field are not same.");
+        return true;
+    }
+
+    var api_request_url = domain + "/api/user/signup/" + input_username + "/" + input_password + "/" + input_password_confirm;
+    var signup_success = false;
+    $.post({
+        url: api_request_url,
+        cache: false,
+        async: false
+    }).done(function(data) {
+        var success = data.success;
+        if (success == "true") {
+            window.location.href = domain;
+            signup_success = true;
+        } else {
+            alert("An error occurred while signup.");
+        }
+    }).fail(function() {
+        alert("Server failed!");
+    });
+    // This makes only website refreshed if login is failed.
+    return !signup_success;
+});
